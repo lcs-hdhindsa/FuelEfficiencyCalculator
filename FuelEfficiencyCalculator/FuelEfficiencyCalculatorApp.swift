68b3a4512ff456590b261aa5642b145b7116b886
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2025-01-20 09:48:35 -0500

    Initial Commit

---
+++ FuelEfficiencyCalculator/FuelEfficiencyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelEfficiencyCalculatorApp.swift
+//  FuelEfficiencyCalculator
+//
+//  Created by Harshan Dhindsa on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct FuelEfficiencyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
