18da04600b055e246c1fe08f029bb085d0011e53
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2025-01-20 09:51:13 -0500

    Initial project organization.

---
+++ FuelEfficiencyCalculator/Views/FuelCalculatorView.swift
@@ -0,0 +1,18 @@
+//
+//  FuelCalculatorView.swift
+//  FuelEfficiencyCalculator
+//
+//  Created by Harshan Dhindsa on 2025-01-20.
+//
+
+import SwiftUI
+
+struct FuelCalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FuelCalculatorView()
+}
